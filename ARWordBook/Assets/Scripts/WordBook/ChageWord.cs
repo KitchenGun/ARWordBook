a1bef919372cfc06fc13b2ef8b788072c850fe14
hdh980217 <74037045+hdh980217@users.noreply.github.com>
2020-11-09 11:04:25 +0900

    WordBookUI

---
+++ ARWordBook/Assets/Scripts/WordBook/ChageWord.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class ChageWord : MonoBehaviour
+{
+    public Text word;
+    private string[] List = { "aaa", "bbb", "ccc" };
+    private int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        word.text = List[i];
+        if (i == List.Length - 1)
+            i = 0;
+        else
+            i++;
+
+        Thread.Sleep(1000);
+    }
+}
