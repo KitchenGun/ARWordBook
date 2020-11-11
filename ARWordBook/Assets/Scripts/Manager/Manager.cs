89b8314a74110bbfee1063b93eef338de2c18d5c
KitchenGun <kitchengun124@gmail.com>
2020-11-11 14:44:02 +0900

    뒤로 돌림

--- ARWordBook/Assets/Scripts/Manager/Manager.cs
+++ ARWordBook/Assets/Scripts/Manager/Manager.cs
@@ -1,19 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Permissions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
     public static Manager instance;
+    public List<string> OcrList { get; set; }
     private int SceneNum=0;
-   
-
     // Start is called before the first frame update
 
     void Start()
     {
-        
         if(instance == null)
         {
             instance=this;
@@ -24,33 +27,47 @@ public class Manager : MonoBehaviour
             Destroy(this.gameObject);
         }
         SceneNum =SceneManager.GetActiveScene().buildIndex;
+
     }
 
     public void LateUpdate()
     {
         #region 뒤로가기 키 입력
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Application.platform == RuntimePlatform.Android)
         {
-            switch (SceneNum)
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                case 0:
-                    Application.Quit();
-                    break;
-                case 1:
-                    Application.Quit();
-                    break;
-                case 2:
-                    SceneManager.LoadScene(1);
-                    break;
+                switch (SceneNum)
+                {
+                    case 0:
+                        Application.Quit();
+                        break;
+                    case 1:
+                        Application.Quit();
+                        break;
+                    case 2:
+                        SceneManager.LoadScene(1);
+                        break;
+                }
             }
         }
         #endregion
     }
 
     #region 현재씬 확인
-    private void OnSceneLoaded()
+    void OnEnable()
     {
+        // 씬 매니저의 sceneLoaded에 체인을 건다.
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Load");
         SceneNum = SceneManager.GetActiveScene().buildIndex;
+    }
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
     #endregion
 
@@ -59,5 +76,14 @@ public class Manager : MonoBehaviour
     {
         SceneManager.LoadScene(2);
     }
+    #endregion
+
+    #region ocr결과를 배열에 저장
+
+    public void SplitManager(string str)
+    {
+        OcrList = str.Split('\n').ToList();
+    }
+
     #endregion
 }
